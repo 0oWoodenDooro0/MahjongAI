1453ab64fbde6c9752fc35c6517201e343ec47eb
User <vincent031525@gmail.com>
2024-12-05 22:14:59 +0800

    Add .gitignore, Tile

---
+++ majhong.py
@@ -0,0 +1,17 @@
+from tile import Tile
+
+
+def check_is_pong(tiles: list[Tile], discard_tile: Tile):
+    pass
+
+
+def check_is_kong(tiles: list[Tile], discard_tile: Tile):
+    pass
+
+
+def check_is_chow(tiles: list[Tile], discard_tile: Tile):
+    pass
+
+
+def check_is_win(tiles: list[Tile], discard_tile: Tile):
+    pass
---
+++ tile.py
@@ -0,0 +1,38 @@
+from enum import IntEnum, auto
+
+
+class Tile(IntEnum):
+    C1 = auto()  # 萬 1-9
+    C2 = auto()
+    C3 = auto()
+    C4 = auto()
+    C5 = auto()
+    C6 = auto()
+    C7 = auto()
+    C8 = auto()
+    C9 = auto()
+    B1 = auto()  # 條 1-9
+    B2 = auto()
+    B3 = auto()
+    B4 = auto()
+    B5 = auto()
+    B6 = auto()
+    B7 = auto()
+    B8 = auto()
+    B9 = auto()
+    D1 = auto()  # 筒 1-9
+    D2 = auto()
+    D3 = auto()
+    D4 = auto()
+    D5 = auto()
+    D6 = auto()
+    D7 = auto()
+    D8 = auto()
+    D9 = auto()
+    W1 = auto()  # 風 東南西北
+    W2 = auto()
+    W3 = auto()
+    W4 = auto()
+    Dragon1 = auto()  # 箭 中發白
+    Dragon2 = auto()
+    Dragon3 = auto()
