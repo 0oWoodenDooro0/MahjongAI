39339c3ceb597f282fe118e594acfde4654cff9a
User <vincent031525@gmail.com>
2024-12-06 21:17:03 +0800

    Add chou pong kong to player

--- player.py
+++ player.py
@@ -4,6 +4,22 @@ from tile import Tile
 class Player:
     def __init__(self):
         self.hand: list[Tile] = []
+        self.decalaration: list[Tile] = []
 
     def draw(self, tile: Tile):
         self.hand.append(tile)
+
+    def discard(self, tile: Tile):
+        pass
+
+    def chou(self, tile: Tile):
+        pass
+
+    def pong(self, tile: Tile):
+        pass
+
+    def kong(self, tile: Tile):
+        pass
+
+    def claim_to_declaration(self):
+        pass
