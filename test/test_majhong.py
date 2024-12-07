9fdce44d42b7afd722ccde3767240be6525b7b7f
uwuWhitecat <aisbn3710@gmail.com>
2024-12-07 15:28:31 +0800

    test_majhong 100% coverage

--- test/test_majhong.py
+++ test/test_majhong.py
@@ -5,19 +5,55 @@ from tile import Tile
 
 class TestMajhong(TestCase):
     def test_deal(self):
-        tile_chow_list1, tile_chow1 = [Tile.C3, Tile.C2], Tile.C1
-        tile_chow_list2, tile_chow2 = [Tile.C3, Tile.C1], Tile.C2
-        tile_chow_list3, tile_chow3 = [Tile.C7, Tile.C8], Tile.C9
-        tile_chow_list4, tile_chow4 = [Tile.C9, Tile.C8], Tile.C8
+        tile_chow_list1 = [Tile.C3, Tile.C2]
+        tile_chow_list2 = [Tile.C3, Tile.C1]
+        tile_chow_list3 = [Tile.C7, Tile.C8]
+        tile_chow_list4 = [Tile.C9, Tile.C7]
 
-        self.assertTrue(check_is_chow(tile_chow_list1, tile_chow1))
-        self.assertTrue(check_is_chow(tile_chow_list2, tile_chow2))
-        self.assertTrue(check_is_chow(tile_chow_list3, tile_chow3))
-        self.assertTrue(check_is_chow(tile_chow_list4, tile_chow4))
+        self.assertTrue(check_is_chow(tile_chow_list1, Tile.C1))
+        self.assertTrue(check_is_chow(tile_chow_list2, Tile.C2))
+        self.assertTrue(check_is_chow(tile_chow_list3, Tile.C9))
+        self.assertTrue(check_is_chow(tile_chow_list4, Tile.C8))
 
-        tile_kong_list, tile_kong = [Tile.D3, Tile.D3, Tile.D3], Tile.D3
-        tile_pong_list, tile_pong = [Tile.D3, Tile.D3], Tile.D3
-        self.assertTrue(check_is_pong(tile_pong_list, tile_pong))
-        self.assertTrue(check_is_kong(tile_pong_list, tile_pong))
-        self.assertFalse(check_is_pong(tile_kong_list, tile_kong))
-        self.assertTrue(check_is_pong(tile_kong_list, tile_kong))
+        tile_kong_list = [Tile.D3, Tile.D3, Tile.D3]
+        tile_pong_list = [Tile.D3, Tile.D3]
+        self.assertFalse(check_is_kong(tile_pong_list, Tile.D3))
+        self.assertTrue(check_is_kong(tile_kong_list, Tile.D3))
+
+        self.assertTrue(check_is_pong(tile_pong_list, Tile.D3))
+        self.assertTrue(check_is_pong(tile_kong_list, Tile.D3))
+        self.assertFalse(check_is_pong([Tile.D3, Tile.D2], Tile.D3))
+
+        tile_win_list1 = [Tile.D3, Tile.D3, Tile.C7, Tile.C7]
+        tile_win_list2 = [
+            Tile.D3,
+            Tile.D3,
+            Tile.D3,
+            Tile.C7,
+            Tile.C7,
+            Tile.C8,
+            Tile.C8,
+            Tile.B7,
+            Tile.B7,
+            Tile.B7,
+        ]
+        tile_win_list3 = [
+            Tile.D1,
+            # Tile.D2,
+            Tile.D3,
+            Tile.C4,
+            Tile.C4,
+            Tile.C4,
+            Tile.B1,
+            Tile.B1,
+        ]
+        self.assertTrue(check_is_win(tile_win_list1, Tile.C7))
+        self.assertFalse(check_is_win(tile_win_list1, Tile.W1))
+
+        self.assertTrue(check_is_win(tile_win_list2, Tile.C8))
+        self.assertTrue(check_is_win(tile_win_list2, Tile.C7))
+        self.assertFalse(check_is_win(tile_win_list2, Tile.B7))
+        self.assertFalse(check_is_win(tile_win_list2, Tile.W1))
+
+        self.assertTrue(check_is_win(tile_win_list3, Tile.D2))
+        self.assertFalse(check_is_win(tile_win_list3, Tile.W1))
