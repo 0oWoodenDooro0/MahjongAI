adb2f78192bf9af92707ed632f73758e6c96c96f
111590007 <t111590007@ntut.org.tw>
2024-12-14 13:01:17 +0800

    修改之前pull request

--- mahjong/game.py
+++ mahjong/game.py
@@ -1,25 +1,23 @@
-from .board import Board
-from .player import Player
+from .board import Board 
+from .player import Player 
+from .tile import Tile 
 
+class Game: 
+    def __init__(self): 
+        self.board = Board() 
+        self.players = [Player(), Player(), Player(), Player()] 
 
-class Game:
-    def __init__(self):
-        self.board = Board()
-        self.players = [Player(), Player(), Player(), Player()]
+    def deal(self): 
+        for _ in range(16): 
+            for player in self.players: 
+                self.board.deal(player) 
 
-    def deal(self):
-        for _ in range(16):
-            for player in self.players:
-                self.board.deal(player)
+    def discard(self, player: Player, tile: Tile): 
+        pass 
 
-    def discard(self, player: Player):
+    def chou(self, player: Player, tile: Tile): 
         pass
-
-    def chou(self, player: Player):
-        pass
-
-    def pong(self, player: Player):
-        pass
-
-    def kong(self, player: Player):
+    def pong(self, player: Player, tile: Tile): 
+        pass    
+    def kong(self, player: Player, tile: Tile): 
         pass
