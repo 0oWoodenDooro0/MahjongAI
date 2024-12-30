77740a069b253fd9f71e2440f1a6c6dfec8b6e77
uwuWhitecat <aisbn3710@gmail.com>
2024-12-30 14:14:59 +0800

    test_tile

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,12 +1,15 @@
 from unittest import TestCase
 
-from mahjong import Tile, check_is_add_kong, check_is_closed_kong, check_is_self_win
+from mahjong import Tile
 from mahjong import (
     check_is_chow,
     check_is_kong,
+    check_is_closed_kong,
+    check_is_add_kong,
     check_is_pong,
     check_is_win,
     check_listen,
+    check_is_self_win
 )
 
 
@@ -18,6 +21,7 @@ class TestMahjong(TestCase):
         tile_chow_list4 = [Tile.C9, Tile.C7]
         tile_chow_list5 = [Tile.B9, Tile.C7]
         tile_chow_list6 = [Tile.C2, Tile.C3, Tile.C4, Tile.C5, Tile.C6]
+        tile_chow_list7 = [Tile.W2, Tile.W3]
 
         result1 = check_is_chow(tile_chow_list1, Tile.C1)
         result2 = check_is_chow(tile_chow_list2, Tile.C2)
@@ -25,6 +29,7 @@ class TestMahjong(TestCase):
         result4 = check_is_chow(tile_chow_list4, Tile.C8)
         result5 = check_is_chow(tile_chow_list5, Tile.C8)
         result6 = check_is_chow(tile_chow_list6, Tile.C4)
+        result7 = check_is_chow(tile_chow_list7, Tile.W1)
         self.assertEqual((Tile.C1, Tile.C2, Tile.C3), result1[0])
         self.assertEqual((Tile.C1, Tile.C2, Tile.C3), result2[0])
         self.assertEqual((Tile.C7, Tile.C8, Tile.C9), result3[0])
@@ -33,6 +38,7 @@ class TestMahjong(TestCase):
         self.assertEqual((Tile.C2, Tile.C3, Tile.C4), result6[0])
         self.assertEqual((Tile.C3, Tile.C4, Tile.C5), result6[1])
         self.assertEqual((Tile.C4, Tile.C5, Tile.C6), result6[2])
+        self.assertIsNone(result7)
 
     def test_check_is_kong(self):
         self.assertEqual(
